c7dae338adfdaaa5b7879ec7849e411d1d94fe7a
Si Beaumont <simon.beaumont@citrix.com>
2013-11-27 11:46:54 +0000

    Update travis scripts

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -30,7 +30,30 @@ eval `opam config -env`
 
 # Post-boilerplate
 make
-git checkout gh-pages
 ./ladder --markdown --title "XenServer Chess Ladder" players games > index.md
-git add index.md
-git commit -m 'Travis auto update'
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Starting to update gh-pages\n"
+
+  #copy data we're interested in to other place
+  cp index.md $HOME/index.md
+
+  #go to home and setup git
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
+
+  #using token clone gh-pages branch
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/simonjbeaumont/elo-ladder.git  gh-pages > /dev/null
+
+  #go into diractory and copy data we're interested in to that directory
+  cd gh-pages
+  cp -f $HOME/index.md .
+
+  #add, commit and push files
+  git add index.md
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Updated gh-pages with latest ladder\n"
+fi
